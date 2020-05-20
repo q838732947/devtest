013a9c5c5da7948851eca87359d69461543f4b0b
chenling <q838732947@qq.com>
2020-05-20 08:42:55 +0800

    update

--- pythons/test_json.py
+++ pythons/test_json.py
@@ -1,5 +1,7 @@
 import json
 
+import pytest
+
 
 class TestJson:
     def test_jsondumps(self):  # json.dumps用于将python对象编码成json字符串
@@ -14,6 +16,5 @@ class TestJson:
         print(a[-1:-5:-1])
 
 
-
-        if __name__ == '__main__':
-            pass
+if __name__ == '__main__':
+    pytest.main()
